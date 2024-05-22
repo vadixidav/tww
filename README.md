7ed84a38608a1857d3177b720faacb9f24830d1c
Geordon Worley <vadixidav@gmail.com>
2024-05-22 18:25:12 -0500

    add some initial code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# tww
-Tokio + WebGPU + Winit
+# tww (Tokio + WebGPU + Winit)
+
+This is a highly opinionated library that you use when you want to use `tokio`, `wgpu`, and `winit`. You interact with `winit` exclusively through async/await so that you never have to worry about event loops again. It will automatically take care of recreating the window surface and rendering resources at the correct times based on the application lifecycle so that you don't need to worry about these details. You provide what capabilities you need to do your rendering and it will take care of the rest.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use core::future::Future;
+use tokio::sync::OnceCell;
+use winit::{application::ApplicationHandler, error::EventLoopError, event_loop::EventLoop};
+
+static WINIT_CONTEXT: OnceCell<WinitContext> = OnceCell::const_new();
+
+/// This function does not return until the window closes and all resources are closed unless an error occurs during runtime.
+///
+/// Pass in `main`, which will be spawned on the `tokio` runtime and act as your entry point.
+pub fn start<F>(main: F) -> Result<(), EventLoopError>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let _rt_guard = rt.enter();
+
+    WINIT_CONTEXT
+        .set(WinitContext {})
+        .ok()
+        .expect("nothing else can initialize WINIT_CONTEXT");
+
+    tokio::spawn(main);
+
+    let mut app = Application {};
+
+    EventLoop::new().unwrap().run_app(&mut app)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Application {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ApplicationHandler for Application {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        todo!()
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        todo!()
     }
 }
+
+struct WinitContext {}
