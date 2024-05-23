91d5edb40ee6a581d606e91ddce24904344f95c4
Geordon Worley <vadixidav@gmail.com>
2024-05-22 20:23:52 -0500

    add some infrastructure for window opening

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 use core::future::Future;
-use tokio::sync::OnceCell;
-use winit::{application::ApplicationHandler, error::EventLoopError, event_loop::EventLoop};
+use snafu::{ResultExt, Snafu};
+use std::sync::Arc;
+use tokio::sync::{mpsc, oneshot, Notify, OnceCell};
+use winit::{
+    application::ApplicationHandler,
+    error::EventLoopError,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy},
+    window::WindowAttributes,
+};
 
-static WINIT_CONTEXT: OnceCell<WinitContext> = OnceCell::const_new();
+const COMMAND_CHANNEL_DEPTH: usize = 64;
+
+static RUNTIME_CONTEXT: OnceCell<RuntimeContext> = OnceCell::const_new();
+
+#[derive(Debug, Snafu)]
+enum CreateWindowError {
+    #[snafu(display("tww failed to open a new window: {source}"))]
+    Os { source: winit::error::OsError },
+}
 
 /// This function does not return until the window closes and all resources are closed unless an error occurs during runtime.
 ///
@@ -18,33 +34,130 @@ where
         .unwrap();
     let _rt_guard = rt.enter();
 
-    WINIT_CONTEXT
-        .set(WinitContext {})
-        .ok()
-        .expect("nothing else can initialize WINIT_CONTEXT");
+    let (commander, commands) = mpsc::channel(COMMAND_CHANNEL_DEPTH);
 
+    RUNTIME_CONTEXT
+        .set(RuntimeContext { commander })
+        .ok()
+        .expect("nothing else can initialize tww::RUNTIME_CONTEXT");
+
+    let event_loop = EventLoop::with_user_event()
+        .build()
+        .expect("couldn't create a winit event loop");
+
+    tokio::spawn(runtime(commands, event_loop.create_proxy()));
     tokio::spawn(main);
 
     let mut app = Application {};
 
-    EventLoop::new().unwrap().run_app(&mut app)
+    event_loop.run_app(&mut app)
+}
+
+async fn runtime(
+    mut commands: mpsc::Receiver<RuntimeCommand>,
+    winit_commander: EventLoopProxy<WinitCommand>,
+) {
+    while let Some(command) = commands.recv().await {
+        match command {
+            RuntimeCommand::CreateWindow {
+                responder,
+                attributes,
+            } => {
+                winit_commander
+                    .send_event(WinitCommand::CreateWindow {
+                        responder,
+                        attributes,
+                    })
+                    .ok()
+                    .expect("winit event loop should not close unless tww initiates it");
+            }
+        }
+    }
+}
+
+enum WinitCommand {
+    CreateWindow {
+        responder: oneshot::Sender<Result<Window, CreateWindowError>>,
+        attributes: WindowAttributes,
+    },
+}
+
+enum RuntimeCommand {
+    CreateWindow {
+        responder: oneshot::Sender<Result<Window, CreateWindowError>>,
+        attributes: WindowAttributes,
+    },
+}
+
+/// This wraps an operating system window.
+///
+/// When dropped, the window closes with eventual consistency. To close
+pub struct Window {
+    commander: mpsc::Sender<WindowCommand>,
+}
+
+impl Window {
+    fn new(
+        event_loop: &ActiveEventLoop,
+        attributes: WindowAttributes,
+    ) -> Result<Self, CreateWindowError> {
+        let _window = Arc::new(event_loop.create_window(attributes).context(OsSnafu)?);
+
+        let (commander, mut commands) = mpsc::channel(COMMAND_CHANNEL_DEPTH);
+        tokio::spawn(async move {
+            while let Some(command) = commands.recv().await {
+                match command {
+                    WindowCommand::Close { finished } => {
+                        todo!()
+                    }
+                }
+            }
+        });
+
+        Ok(Self { commander })
+    }
+
+    pub async fn close(&self) {
+        todo!()
+    }
+}
+
+enum WindowCommand {
+    Close { finished: Notify },
 }
 
 struct Application {}
 
-impl ApplicationHandler for Application {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+impl ApplicationHandler<WinitCommand> for Application {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {
+        todo!()
+    }
+
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
         todo!()
     }
 
     fn window_event(
         &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
+        _event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        _event: WindowEvent,
     ) {
         todo!()
     }
+
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: WinitCommand) {
+        match event {
+            WinitCommand::CreateWindow {
+                responder,
+                attributes,
+            } => {
+                responder.send(Window::new(event_loop, attributes)).ok();
+            }
+        }
+    }
 }
 
-struct WinitContext {}
+struct RuntimeContext {
+    commander: mpsc::Sender<RuntimeCommand>,
+}
